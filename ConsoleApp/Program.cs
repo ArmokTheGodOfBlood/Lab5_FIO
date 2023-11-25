3bb792ea834a6c4b094d5bd35b5aa90a6b6166e9
SHveygertKK <SHveygertKK@410D-7>
2023-11-25 13:59:35 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        WebDriver driver = new FirefoxDriver();
+        driver.Navigate().GoToUrl("https://www.globalsqa.com/angularJs-protractor/SimpleCalculator/");
+        CalcPage page = new CalcPage(driver);
+        Console.WriteLine(page.aField.GetAttribute("value"));
+        Console.WriteLine(page.bField.GetAttribute("value"));
+        driver.Close();
+    }
+}
+
+public class CalcPage
+{
+    private WebDriver driver;
+    public CalcPage(WebDriver driver)
+    {
+        this.driver = driver;
+
+        aField = driver.FindElement(By.CssSelector("[ng-model='a']"));
+        bField = driver.FindElement(By.CssSelector("[ng-model='b']"));
+    }
+
+    public IWebElement aField;
+    public IWebElement bField;
+}
---
+++ Lab5_FIO/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Lab5_FIO
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
