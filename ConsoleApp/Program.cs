eddaf3a66960e41a0992ddef069e18bb696a3cfc
shveygertkk <shveygertkk@410D-8>
2023-12-09 15:03:27 +0700

    уээээ

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,32 +1,86 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
-using System.Security.Cryptography.X509Certificates;
 
-internal class Program
+namespace ConsoleApp
 {
-    private static void Main(string[] args)
+    public static class Program
     {
-        WebDriver driver = new FirefoxDriver();
-        driver.Navigate().GoToUrl("https://www.globalsqa.com/angularJs-protractor/SimpleCalculator/");
-        CalcPage page = new CalcPage(driver);
-        driver.Close();
-    }
-}
 
-public class CalcPage
-{
-    private WebDriver driver;
-    public CalcPage(WebDriver driver)
-    {
-        this.driver = driver;
-
-        IWebElement aElm = driver.FindElement(aField);
-        IWebElement bElm = driver.FindElement(aField);
-
-        Console.WriteLine(aElm.GetAttribute("value"));
-        Console.WriteLine(bElm.GetAttribute("value"));
+        public static CalcPage page;
+        public static void Main()
+        {
+            SetupPageModel();
+        }
+        public static void SetupPageModel()
+        {
+            WebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.globalsqa.com/angularJs-protractor/SimpleCalculator/");
+            page = new CalcPage(driver);
+            driver.Close();
+        }
     }
 
-    public By aField = By.CssSelector("[ng-model='a']");
-    public By bField = By.CssSelector("[ng-model='b']");
+    public class CalcPage
+    {
+        private IWebDriver driver;
+
+        public By aField = By.CssSelector("[ng-model='a']");
+        public By bField = By.CssSelector("[ng-model='b']");
+
+        public By aPlus = By.CssSelector("[ng-click='inca()']");
+        public By aMinus = By.CssSelector("[ng-click='deca()']");
+        public By bPlus = By.CssSelector("[ng-click='incb()']");
+        public By bMinus = By.CssSelector("[ng-click='decb()']");
+
+        public By combobox = By.CssSelector("[ng-model='operation']");
+        public By opPlus = By.CssSelector("[value='+']");
+        public By opMinus = By.CssSelector("[value='-']");
+        public By opMult = By.CssSelector("[value='*']");
+        public By opDiv = By.CssSelector("[value='/']");
+
+        public By res = By.CssSelector("[class='result ng-binding']");
+
+        public CalcPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public CalcPage SetInputA(string s)
+        {
+            driver.FindElement(aField).SendKeys(s);
+            return this;
+        }
+        public CalcPage SetInputB(string s)
+        {
+            driver.FindElement(aField).SendKeys(s);
+            return this;
+        }
+
+        public string GetInputA()
+        {
+            return driver.FindElement(aField).GetAttribute("value");
+        }
+        public string GetInputB()
+        {
+            return driver.FindElement(bField).GetAttribute("value");
+        }
+
+        public void ClickIncA()
+        {
+            driver.FindElement(aPlus).Click();
+        }
+        public void ClickDecA()
+        {
+            driver.FindElement(aMinus).Click();
+        }
+        public void ClickIncB()
+        {
+            driver.FindElement(bPlus).Click();
+        }
+        public void ClickDecB()
+        {
+            driver.FindElement(bMinus).Click();
+        }
+    }
 }
--- Lab5_FIO/UnitTest1.cs
+++ Lab5_FIO/UnitTest1.cs
@@ -1,11 +1,54 @@
+using ConsoleApp;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using static ConsoleApp.Program;
 namespace Lab5_FIO
 {
-    public class UnitTest1
+    public class UnitTest1: IDisposable
     {
-        [Fact]
-        public void Test1()
+        IWebDriver driver;
+        private CalcPage page;
+        public UnitTest1()
         {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.globalsqa.com/angularJs-protractor/SimpleCalculator/");
+            page = new CalcPage(driver);
+        }
 
+        public void Dispose()
+        {
+            driver.Close();
+            driver.Dispose();
+        }
+
+
+        [Fact]
+        public void TestIncrementA()
+        {
+            page.ClickIncA();
+            string val = page.GetInputA();
+            Assert.Equal("1", val);
+        }
+        [Fact]
+        public void TestIncrementB()
+        {
+            page.ClickIncB();
+            string val = page.GetInputB();
+            Assert.Equal("1", val);
+        }
+        [Fact]
+        public void TestDecrementA()
+        {
+            page.ClickDecA();
+            string val = page.GetInputA();
+            Assert.Equal("-1", val);
+        }
+        [Fact]
+        public void TestDecrementB()
+        {
+            page.ClickDecB();
+            string val = page.GetInputB();
+            Assert.Equal("-1", val);
         }
     }
 }
